64aa6a983449c572bbbcb46cca289ab6b261de59
kanishka sahu <kanishkasahu005@gmail.com>
2020-05-15 07:50:25 +0530

    careate and add fun

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,14 +1,67 @@
 import React , {Component} from 'react';
+import axios from 'axios';
 
 
-export default class Createusers extends Component {
-    render(){
-        return (
-            <div>
-                <p>
-                    Welcome to craete user Component !
-                </p>
-            </div>
-        )
+
+export default class CreateUser extends Component {
+    constructor(props){
+        super(props);
+
+        this.onCreateuser = this.onCreateuser.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
+        this.state = {
+            username: ''
+        }
+
+        
     }
+
+
+
+
+onCreateuser(e) {
+    this.setState({
+        username: e.target.value
+    })
+}
+
+onSubmit(e) {
+    e.preventDefault();
+    console.log('user-created')
+    const user = {
+        username: this.state.username
+}
+console.log(user);
+
+axios.post('http://localhost:5000/users/add', user)
+.then(res => console.log(res.data));
+
+this.setState({
+    username: ''
+  })
+}
+
+render(){
+    return (
+        <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onCreateuser}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+
+    )
+}
 }
